a75478f90bd1f9def7e1b2e07ff831aac969773a
alosgi <osorio.alejandra@correounivalle.edu.co>
2024-11-08 02:08:49 -0500

    Se creo la funcion muchoMayorQue

--- app/src/main/scala/taller/conjuntosDifusos.scala
+++ app/src/main/scala/taller/conjuntosDifusos.scala
@@ -14,4 +14,17 @@ class ConjuntosDifusos {
         s(elemen)
     }
 
+    def  muchoMayorQue(a: Int, m:Int): ConjuntoDifuso = {
+        
+        //x funciona para evaluar el conjunto difuso
+        def mma(x: Int): Double={
+            
+            if (x <= a) 0.0
+            else if (x > a && x <= m) (x-a).toDouble/(m-a).toDouble 
+            else 1.0
+        
+        }
+        mma
+    }
+
 }
