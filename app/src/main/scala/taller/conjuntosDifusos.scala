09ecc1070b0fc1c089e5162d6f0260876376f69a
cardona1204 <manuelcardona1204@gmail.com>
2024-11-08 11:58:06 -0500

    Agregamos funciones restantes y otros tests para estas mismas

--- app/src/main/scala/taller/conjuntosDifusos.scala
+++ app/src/main/scala/taller/conjuntosDifusos.scala
@@ -1,7 +1,9 @@
 package taller
+import scala.math.pow
+import scala.math.max
+import scala.math.min
 
-
-class ConjuntosDifusos {
+class ConjDifusos {
 
     type ConjuntoDifuso = Int => Double
 
@@ -14,17 +16,29 @@ class ConjuntosDifusos {
         s(elemen)
     }
 
+
     def  muchoMayorQue(a: Int, m:Int): ConjuntoDifuso = {
         
         //x funciona para evaluar el conjunto difuso
         def mma(x: Int): Double={
             
             if (x <= a) 0.0
-            else if (x > a && x <= m) (x-a).toDouble/(m-a).toDouble 
+            else if (x > a && x <= m) (x-a).toDouble/(m-a).toDouble // (x-a)/(m-a)
             else 1.0
         
         }
         mma
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+    
+
+    def grande(d: Int, e: Int): ConjuntoDifuso = {
+        def auxGrande(x: Int): Double = {
+            math.pow(x.toDouble / (x + d).toDouble, e.toDouble)
+        }
+        auxGrande 
     }
 
     def complemento(s: ConjuntoDifuso): ConjuntoDifuso = {
@@ -41,15 +55,15 @@ class ConjuntosDifusos {
         auxUnion
     }
 
-    
-    def Igualdad(cd1: ConjuntoDifuso, cd2: ConjuntoDifuso): Boolean = {
-        def AuxIgualdad(x: Int): Boolean = {
-            if (x>1000) true 
-            else if (cd1(x) != cd2(x)) false
-            else AuxIgualdad(x+1)
+    def Interseccion(cd1: ConjuntoDifuso, cd2: ConjuntoDifuso): ConjuntoDifuso = {
+        def auxInterseccion(x: Int): Double = {
+            math.min(cd1(x), cd2(x))
         }
-        AuxIgualdad(0)
+        auxInterseccion
     }
+
+    //-----------------------------------------------------------------------------------
+
 
     def Inclusion (cd1: ConjuntoDifuso, cd2: ConjuntoDifuso): Boolean = {
         def AuxInclusion(x: Int): Boolean = {
@@ -60,5 +74,15 @@ class ConjuntosDifusos {
         }
         AuxInclusion(0)//hasta que mi iterador llegue a 1001
     }
+
+    def Igualdad(cd1: ConjuntoDifuso, cd2: ConjuntoDifuso): Boolean = {
+        def AuxIgualdad(x: Int): Boolean = {
+            if (x>1000) true
+            else if (cd1(x) != cd2(x)) false
+            else AuxIgualdad(x+1)
+        }
+        AuxIgualdad(0)
+    }
+    
 
 }
