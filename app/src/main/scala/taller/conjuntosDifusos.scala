518c2da54d5945c668fd195646309ebf7933979a
alosgi <osorio.alejandra@correounivalle.edu.co>
2024-11-08 01:44:38 -0500

    Se definio un conjunto difuso

--- app/src/main/scala/taller/conjuntosDifusos.scala
+++ app/src/main/scala/taller/conjuntosDifusos.scala
@@ -3,4 +3,11 @@ package taller
 
 class ConjuntosDifusos {
 
+    type ConjuntoDifuso = Int => Double
+
+    //Para poder definir un conjunto difuso, se debe definir una función que reciba un entero y devuelva un double
+    object ConjuntoDifuso {
+        def apply(f: Int => Double): ConjuntoDifuso = f
+    }
+
 }
